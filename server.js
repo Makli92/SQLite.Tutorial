39b78725a9a237a523fe706ba7b0aedc3735c4bc
maklitah <kostahiri@gmail.com>
2018-01-26 22:25:44 +0200

    Install cloud-env

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').load();
-}
+var config = require('cloud-env')
 
 var express = require('express');
 var app = express();
 const MongoClient = require('mongodb').MongoClient;
 var uri = "mongodb://DonMakli:Fivoulis92%5E%5E@cluster0-shard-00-00-cvacf.mongodb.net:27017,cluster0-shard-00-01-cvacf.mongodb.net:27017,cluster0-shard-00-02-cvacf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 var db;
-
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 MongoClient.connect(uri, (err, client) => {
   if (err) throw err;
@@ -27,6 +22,6 @@ app.get('/chapters', (request, response) => {
 
 })
 
-app.listen(server_port, server_ip_address, () => {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+app.listen(config.PORT, config.IP, function () {
+  console.log("Listening on "+config.IP+", port "+config.PORT)
 });
