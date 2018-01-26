2e1e76fba732199c89b026826e47b5a9add7f5c3
maklitah <kostahiri@gmail.com>
2018-01-26 20:49:30 +0200

    Add openshift constants

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const MongoClient = require('mongodb').MongoClient;
 var uri = "mongodb://DonMakli:Fivoulis92%5E%5E@cluster0-shard-00-00-cvacf.mongodb.net:27017,cluster0-shard-00-01-cvacf.mongodb.net:27017,cluster0-shard-00-02-cvacf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 var db;
 
-const PORT = process.env.PORT || 3000;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 MongoClient.connect(uri, (err, client) => {
   if (err) throw err;
@@ -26,6 +27,6 @@ app.get('/chapters', (request, response) => {
 
 })
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.listen(server_port, server_ip_address, () => {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
