dd134730b3c8f0fb3c18f45e9d8a9605c2619a48
maklitah <kostahiri@gmail.com>
2018-01-26 20:21:43 +0200

    Dot-env installed, port provided as env param

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-
+require('dotenv').load();
 var express = require('express');
 var app = express();
 const MongoClient = require('mongodb').MongoClient;
 var uri = "mongodb://DonMakli:Fivoulis92%5E%5E@cluster0-shard-00-00-cvacf.mongodb.net:27017,cluster0-shard-00-01-cvacf.mongodb.net:27017,cluster0-shard-00-02-cvacf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 var db;
+
+const PORT = process.env.PORT || 3000;
 
 MongoClient.connect(uri, (err, client) => {
   if (err) throw err;
@@ -21,10 +23,6 @@ app.get('/chapters', (request, response) => {
 
 })
 
-var server = app.listen(8081, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Example app listening at http://%s:%s", host, port)
-
-})
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
